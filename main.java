3d66caf020b751902287a89ce58c3321c25fa5c2
MatildeS <matilde.sequeira@ubi.pt>
2024-12-13 09:20:31 +0000

    classe main

---
+++ main.java
@@ -0,0 +1,19 @@
+public class menuStore () {
+    public static void main(String[] args) {
+
+    }
+        do
+
+        {
+            switch op:
+        case 1:
+            break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }while(op !=4)
+    }
+}
