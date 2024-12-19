d64119bba5f6ac2d42848f5d89c1a9a229140392
MatildeS <matilde.sequeira@ubi.pt>
2024-12-19 11:33:01 +0000

    WIP on main
    menuEmpregado, menuProduto

--- src/ClassesMenu/FuncoesMenu.java
+++ src/ClassesMenu/FuncoesMenu.java
@@ -6,6 +6,7 @@ public class FuncoesMenu {
         System.out.println("1 - Visualizar catálogo");
         System.out.println("2 - Fazer Compra");
         System.out.println("3 - Sair");
+        System.out.println("Escolha uma opção:");
 
         int op = 0;
         while (true) {
@@ -16,6 +17,7 @@ public class FuncoesMenu {
                     break;
                 case 2:
                     System.out.println("Fazer Compra");
+
                     break;
                 case 3:
                     System.out.println("Saí do menu 1");
@@ -23,10 +25,12 @@ public class FuncoesMenu {
             }
         }
     }
+
     public static void menu2() {
         System.out.println("1 - Produtos");
         System.out.println("2 - Empregados");
         System.out.println("3 - Sair");
+        System.out.println("Escolha uma opção:");
 
         int op = 0;
         while (true) {
@@ -34,9 +38,11 @@ public class FuncoesMenu {
             switch (op) {
                 case 1:
                     System.out.println("Produtos");
+                    Menu_Produtos.main();
                     break;
                 case 2:
                     System.out.println("Empregados");
+                    Menu_Empregados.main();
                     break;
                 case 3:
                     System.out.println("sair do menu 2");
@@ -44,10 +50,12 @@ public class FuncoesMenu {
             }
         }
     }
+
     public static void menu3() {
         System.out.println("1 - Vendas");
         System.out.println("2 - Clientes");
         System.out.println("3 - Sair");
+        System.out.println("Escolha uma opção:");
 
         int op = 0;
         while (true) {
@@ -67,7 +75,75 @@ public class FuncoesMenu {
 
     }
 
-    public static void catalogo() {
+    public class Menu_Empregados {
+        public static void main_Empregados() {
+            System.out.println("1 - Adicionar Empregados");
+            System.out.println("2 - Atualizar Empregados ");
+            System.out.println("3 - Visualizar Empregados ");
+            System.out.println("4 - Eliminar Empregados ");
+            System.out.println("5 - Sair");
+            System.out.println("Escolha uma opção:");
+        }
 
+        public static void main() {
+            int op = 0;
+            do {
+                main_Empregados();
+                op = Ler.umInt();
+                switch (op) {
+                    case 1:
+                        System.out.println("Adicionar Empregados");
+                        break;
+                    case 2:
+                        System.out.println("Atualizar Empregados");
+                        break;
+                    case 3:
+                        System.out.println("Visualizar Empregados");
+                        break;
+                    case 4:
+                        System.out.println("Eliminar Empregados");
+                        break;
+                    case 5:
+                        System.out.println("Sair");
+                        return;
+                }
+            } while (true);
+        }
+    }
+
+    public class Menu_Produtos {
+        public static void main_Produtos() {
+            System.out.println("1 - Adicionar Produtos");
+            System.out.println("2 - Atualizar Produtos ");
+            System.out.println("3 - Visualizar Produtos ");
+            System.out.println("4 - Eliminar Produtos ");
+            System.out.println("5 - Sair");
+            System.out.println("Escolha uma opção:");
+        }
+
+        public static void main() {
+            int op = 0;
+            do {
+                main_Produtos();
+                op = Ler.umInt();
+                switch (op) {
+                    case 1:
+                        System.out.println("Adicionar Produtos");
+                        break;
+                    case 2:
+                        System.out.println("Atualizar Produtos");
+                        break;
+                    case 3:
+                        System.out.println("Visualizar Produtos");
+                        break;
+                    case 4:
+                        System.out.println("Eliminar Produtos");
+                        break;
+                    case 5:
+                        System.out.println("Sair");
+                        return;
+                }
+            } while (true);
+        }
     }
 }
