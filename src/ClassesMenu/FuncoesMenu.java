91485daefb9fc5df253061e213019f3bb9fc39f8
Alessandra Delgado <alessandra.delgado@ubi.pt>
2024-12-20 15:00:48 +0000

    added files, changed main class

--- src/ClassesMenu/FuncoesMenu.java
+++ src/ClassesMenu/FuncoesMenu.java
@@ -1,5 +1,11 @@
 package src.ClassesMenu;
+
+import src.Classes_Loja.Animal;
 import src.Input.Ler;
+import src.Main;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class FuncoesMenu {
     public static void menu1() {
@@ -23,6 +29,7 @@ public class FuncoesMenu {
             }
         }
     }
+
     public static void menu2() {
         System.out.println("1 - Produtos");
         System.out.println("2 - Empregados");
@@ -44,6 +51,7 @@ public class FuncoesMenu {
             }
         }
     }
+
     public static void menu3() {
         System.out.println("1 - Vendas");
         System.out.println("2 - Clientes");
@@ -67,7 +75,96 @@ public class FuncoesMenu {
 
     }
 
-    public static void catalogo() {
+    public static void catalogMenu() {
+        System.out.println("1 - Animais");
+        System.out.println("2 - Cosmeticos");
+        System.out.println("3 - Comida");
+        System.out.println("4 - Habitat");
+        System.out.println("5 - Higiene");
+        System.out.println("6 - Medicamentos");
+        System.out.println("7 - Sair");
+        System.out.println("? - ?");
+
+
+        char op = Ler.umChar();
+        do {
+            switch (op) {
+                case '1':
+                    System.out.println("Animais");
+                    break;
+                case '2':
+                    System.out.println("Cosmeticos");
+                    break;
+                case '3':
+                    System.out.println("Comida");
+                    break;
+                case '4':
+                    System.out.println("Habitat");
+                    break;
+                case '5':
+                    System.out.println("Medicamentos");
+                    break;
+                case '6':
+                    System.out.println("Higiene");
+                    break;
+                case '7':
+                    System.out.println("Sair");
+                    return;
+                case '!':
+                    System.out.println("Menu Secreto");
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+                    break;
+            }
+        } while (true);
 
     }
-}
+
+    public static void animalTypes() {
+        System.out.println("1 - Aéreos");
+        System.out.println("2 - Terrestres");
+        System.out.println("3 - Aquáticos");
+        System.out.println("4 - Sair");
+
+        int op = 0;
+        while (true) {
+            op = Ler.umInt();
+            switch (op) {
+                case 1:
+                    System.out.println("Aéreos");
+                    Main.animals_i.remove(0);
+                    Main.animals_i.addAll(0, (ArrayList<Animal>) readFile("src/Files_Products/Animal/Imaginary/Air.dat"));
+                    break;
+                case 2:
+                    System.out.println("Terrestres");
+                    Main.animals_i.remove(1);
+                    Main.animals_i.addAll(1, (ArrayList<Animal>) readFile("src/Files_Products/Animal/Imaginary/Land.dat"));
+                    break;
+                case 3:
+                    System.out.println("Aquáticos");
+                    Main.animals_i.remove(2);
+                    Main.animals_i.addAll(2, (ArrayList<Animal>) readFile("src/Files_Products/Animal/Imaginary/Water.dat"));
+                    break;
+                case 4:
+                    System.out.println("Sair");
+                    return;
+            }
+
+        }
+        public static ArrayList<?> readFile (String path){
+            ArrayList<?> arr = new ArrayList<>();
+            try {
+                ObjectInputStream is = new ObjectInputStream(new FileInputStream(path));
+                arr = (ArrayList<?>) is.readObject();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+
+            return arr;
+        }
+    }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,38 @@
 package src;
 
 import src.ClassesMenu.Menu;
+import src.Classes_Loja.*;
+
+import java.util.ArrayList;
 
 public class Main {
+    public static ArrayList<Animal> animals_r;
+    public static ArrayList<Animal> animals_i;
+    public static ArrayList<Product> cosmetics;
+    public static ArrayList<Product> food;
+    public static ArrayList<Product> habitat;
+    public static ArrayList<Product> hygiene;
+    public static ArrayList<Product> medicine;
+    public static ArrayList<Product> secret;
+
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        init();
+
 
         Menu.main();
+    }
+
+    public static void init(){
+        ArrayList<ArrayList<Animal>> animals_r = new ArrayList<>();
+        ArrayList<ArrayList<Animal>> animals_i = new ArrayList<>();
+        ArrayList<Product> cosmetics = new ArrayList<>();
+        ArrayList<Product> food = new ArrayList<>();
+        ArrayList<Product> habitat = new ArrayList<>();
+        ArrayList<Product> hygiene = new ArrayList<>();
+        ArrayList<Product> medicine = new ArrayList<>();
+        ArrayList<Product> secret = new ArrayList<>();
+
     }
 }
