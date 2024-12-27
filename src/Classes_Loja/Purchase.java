d693716513b6b760e89ab0dbea5962b7dcd4e131
AnaBDSilva <ana.bd.silva@ubi.pt>
2024-12-27 13:03:36 +0000

    added quant_prod_sold in purchase

--- src/Classes_Loja/Purchase.java
+++ src/Classes_Loja/Purchase.java
@@ -1,9 +1,11 @@
 package src.Classes_Loja;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Purchase {
     private static int last_id = 0;
+    private static HashMap<Integer, Integer> quant_prod_sold;
     private int id;
     private int total;
     private ArrayList<Product> products;
@@ -12,6 +14,7 @@ public class Purchase {
         this.id = ++last_id;
         this.total = 0;
         products = new ArrayList<>();
+        quant_prod_sold = new HashMap<>();
     }
 
     public int getId() {
@@ -26,6 +29,10 @@ public class Purchase {
         return new ArrayList<>(this.products);
     }
 
+    public static HashMap<Integer, Integer> getQuant_prod_sold() {
+        return quant_prod_sold;
+    }
+
     public void setId(int id) {
         this.id = id;
     }
@@ -34,22 +41,44 @@ public class Purchase {
         this.products = products;
     }
 
+    public static void setQuant_prod_sold(HashMap<Integer, Integer> quant_prod_sold) {
+        Purchase.quant_prod_sold = new HashMap<>(quant_prod_sold);
+    }
+
     public void addProduct(Product p) {
         this.products.add(p);
         this.total += (int) p.getPrice();
+        if ( quant_prod_sold.containsKey(p.getId()) ) {
+            quant_prod_sold.put(p.getId(), quant_prod_sold.get(p.getId()) + 1);
+        }else {
+            quant_prod_sold.put(p.getId(), 1);
+        }
     }
 
     public void removeProduct(Product p) {
         this.products.remove(p);
         this.total -= (int) p.getPrice();
+        if ( quant_prod_sold.get(p.getId()) != 1 ) {
+            quant_prod_sold.put(p.getId(), quant_prod_sold.get(p.getId()) - 1);
+        }else {
+            quant_prod_sold.remove(p.getId(), quant_prod_sold.get(p.getId()));
+        }
     }
 
     public void removeProduct(int id) {
         this.products.remove(id);
         this.total -= (int) this.products.get(id).getPrice();
+        if ( quant_prod_sold.get(id) != 1 ) {
+            quant_prod_sold.put(id, quant_prod_sold.get(id) - 1);
+        }else {
+            quant_prod_sold.remove(id, quant_prod_sold.get(id));
+        }
     }
 
     public void removeAllProducts() {
+        for ( Product p : this.products ) {
+            quant_prod_sold.remove(p.getId());
+        }
         this.products.clear();
         this.total = 0;
     }
